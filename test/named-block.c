9bb1c1e4ad48292a428c093abc51415bca100bb4
Ethan Pailes <ethanpailes@gmail.com>
2015-11-14 12:50:48 -0500

    Finished named types support.

--- test/named-block.c
+++ test/named-block.c
@@ -11,19 +11,23 @@ int main(int argc, char *argv[])
 {
   FILE *f = fopen(TMP_FILE, "w");
 
-  inner t;
+  outer t;
   t.fieldOne = 34;
   t.fieldTwo = 48;
-  inner_write(&t, f);
+  t.nested.fieldOne = 44;
+  t.nested.fieldTwo = 9324;
+  outer_write(&t, f);
  
   fclose(f);
   FILE *in = fopen(TMP_FILE, "r");
 
-  inner t2;
-  inner_read(&t2, in);
+  outer t2;
+  outer_read(&t2, in);
 
   assert(t2.fieldOne == 34);
   assert(t2.fieldTwo == 48);
+  assert(t.nested.fieldOne == 44);
+  assert(t.nested.fieldTwo == 9324);
 
   execl("rm", "-f", TMP_FILE, NULL);
 
